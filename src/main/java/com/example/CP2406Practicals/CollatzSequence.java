0a36c8d1f9ff9bcef36a54e8ba17bf394e7df716
inder <inderjotsingh@my.jcu.edu.au>
2023-02-24 13:26:01 +1000

    Added ComputeAverage

--- src/main/java/com/example/CP2406Practicals/CollatzSequence.java
+++ src/main/java/com/example/CP2406Practicals/CollatzSequence.java
@@ -4,7 +4,7 @@ public class CollatzSequence {
     public static void main(String[] args) {
         int N;
         System.out.print("Type any positive integer: ");
-        N = com.example.demo2.TextIO.getlnInt();
+        N = TextIO.getlnInt();
         while (N!=1) {
             if (N%2 ==0){
                  N=N/2;
