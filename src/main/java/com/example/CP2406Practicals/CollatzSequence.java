8c328bd621cc204719a099f87a9619ef2289387c
inder <inderjotsingh@my.jcu.edu.au>
2023-02-24 13:12:59 +1000

    Added CollatzSequence

---
+++ src/main/java/com/example/CP2406Practicals/CollatzSequence.java
@@ -0,0 +1,19 @@
+package com.example.CP2406Practicals;
+
+public class CollatzSequence {
+    public static void main(String[] args) {
+        int N;
+        System.out.print("Type any positive integer: ");
+        N = com.example.demo2.TextIO.getlnInt();
+        while (N!=1) {
+            if (N%2 ==0){
+                 N=N/2;
+            }
+            else {
+                N = 3*N +1;
+            }
+            System.out.println(N);
+
+        }
+    }
+}
